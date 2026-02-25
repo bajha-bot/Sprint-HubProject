cf9f6f8eab423237a6f78c36f39dd445a2ee3ed5
Barun Kumar Jha <BJ121@tmw.com>
2026-02-25 15:38:31 +0530

    Updates

--- src/constants/apiToken.js
+++
@@ -1,9 +0,0 @@
-import { STORAGE_KEYS } from './storageKeys';
-
-export const API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuaXN1bS11bml2ZXJzaXR5LWFwcCIsImNvbnN1bWVySWQiOiJuaXN1bS11bml2ZXJzaXR5IiwiaWF0IjoxNzcyMDExODk3LCJleHAiOjE3NzIwMTU0OTd9.r1xzpCzkO_NE8ttLDodK6Cn9gu1l1mVL_iJ5-2QeCjU";
-
-export const setTempToken = () => {
-  localStorage.setItem(STORAGE_KEYS.ACCESS_TOKEN, API_TOKEN);
-  localStorage.setItem(STORAGE_KEYS.TOKEN_EXPIRY, Date.now() + 3600000);
-};
-  
--- src/utils/redirectToLogin.js
+++
@@ -1,3 +0,0 @@
-export const redirectToLogin = () => {
-//   window.location.href = "/login";
-};

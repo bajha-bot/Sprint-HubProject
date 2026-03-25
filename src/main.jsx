613cc4fba279122a52354804e85461b82440fd36
Barun Kumar Jha <BJ121@tmw.com>
2026-03-25 12:22:18 +0530

    remove comment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 
-// const basename = import.meta.env.PROD ? '/Sprint-HubProject' : '/';
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>

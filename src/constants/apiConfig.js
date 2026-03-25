d32982b5381c89fa12cbd56d650c57f727c57254
Barun Kumar Jha <BJ121@tmw.com>
2026-03-25 14:55:50 +0530

    add env

--- src/constants/apiConfig.js
+++ src/constants/apiConfig.js
@@ -1,9 +1,18 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://prodbe-myteam.mynisum.com';
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://prodbe-myteam.mynisum.com';
 
-export const API_BASE_URL = import.meta.env.PROD ? BASE_URL : '';
+// export const API_BASE_URL = import.meta.env.PROD ? BASE_URL : '';
+
+// export const API_ENDPOINTS = {
+//   GENERATE_TOKEN: `${import.meta.env.PROD ? BASE_URL : ''}/myTeam/auth/generate-token`,
+//   REFRESH_TOKEN: `${import.meta.env.PROD ? BASE_URL : ''}/myTeam/auth/refresh-token`,
+//   GET_ALL_EMPLOYEES: `${import.meta.env.PROD ? BASE_URL : ''}/myTeam/open-apis/getAllEmployees`,
+// };
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// export const API_BASE_URL = BASE_URL;
 
 export const API_ENDPOINTS = {
-  GENERATE_TOKEN: `${import.meta.env.PROD ? BASE_URL : ''}/myTeam/auth/generate-token`,
-  REFRESH_TOKEN: `${import.meta.env.PROD ? BASE_URL : ''}/myTeam/auth/refresh-token`,
-  GET_ALL_EMPLOYEES: `${import.meta.env.PROD ? BASE_URL : ''}/myTeam/open-apis/getAllEmployees`,
+  GENERATE_TOKEN: `${BASE_URL}/myTeam/auth/generate-token`,
+  REFRESH_TOKEN: `${BASE_URL}/myTeam/auth/refresh-token`,
+  GET_ALL_EMPLOYEES: `${BASE_URL}/myTeam/open-apis/getAllEmployees`,
 };

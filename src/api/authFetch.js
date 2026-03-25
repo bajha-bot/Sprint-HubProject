ef5ca49dd0c9e4fb5bb7b59d2f927c1822fc47b6
Barun Kumar Jha <BJ121@tmw.com>
2026-03-25 15:52:04 +0530

    node api integrate

--- src/api/authFetch.js
+++ src/api/authFetch.js
@@ -5,13 +5,37 @@ export const authFetch = async (url, options = {}) => {
 
   if (!token) throw new Error("Unauthorized");
 
-  return fetch(url, {
-    ...options,
-    mode: 'cors',
-    headers: {
-      ...options.headers,
-      'Content-Type': 'application/json',
-      AppToken: token,
-    },
+  return fetch(`http://localhost:5000/api/employees`, {
+    // ...options,
+    // mode: 'cors',
+    // headers: {
+    //   ...options.headers,
+    //   'Content-Type': 'application/json',
+    //   AppToken: token,
+    // },
   });
 };
+
+
+
+
+
+
+
+// import { getToken } from '../constants/apiToken';
+
+// export const authFetch = async (url, options = {}) => {
+//   const token = await getToken();
+
+//   if (!token) throw new Error("Unauthorized");
+
+//   return fetch(url, {
+//     ...options,
+//     mode: 'cors',
+//     headers: {
+//       ...options.headers,
+//       'Content-Type': 'application/json',
+//       AppToken: token,
+//     },
+//   });
+// };
